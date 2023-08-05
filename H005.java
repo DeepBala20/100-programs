1298dbef914abf6df929c4372dda844adcb57fd9
Deep Bala <114481589+DeepBala20@users.noreply.github.com>
2023-08-05 09:34:15 +0530

    Add files via upload

---
+++ H005.java
@@ -0,0 +1,10 @@
+//5. WAP to find a summation of a digit of a given number. (Iterative and recursive) 
+public class H005 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Sum{
+    
+}
